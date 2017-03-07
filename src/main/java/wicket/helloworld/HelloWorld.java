1c77ce1aa9dbc1aa60f978550ba1820883aa79de
Ariana Gozar-Manu <ariana.gozar-manu@artsoft-consult.ro>
2017-03-07 08:39:58 +0200

    Rezolvare problema localizarii resurselor

--- src/main/java/wicket/helloworld/HelloWorld.java
+++ src/main/java/wicket/helloworld/HelloWorld.java
@@ -1,13 +1,17 @@
 package wicket.helloworld;
 
 import org.apache.wicket.markup.head.CssHeaderItem;
+import org.apache.wicket.markup.head.CssReferenceHeaderItem;
 import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.markup.head.JavaScriptHeaderItem;
 import org.apache.wicket.markup.head.JavaScriptReferenceHeaderItem;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.repeater.RepeatingView;
+import org.apache.wicket.request.resource.ContextRelativeResourceReference;
 import org.apache.wicket.request.resource.CssResourceReference;
 import org.apache.wicket.request.resource.JavaScriptResourceReference;
+import org.apache.wicket.request.resource.PackageResourceReference;
 
 public class HelloWorld extends WebPage {
 	
@@ -42,8 +46,8 @@ public class HelloWorld extends WebPage {
 	
 	@Override
 	public void renderHead(IHeaderResponse response) {
-	    response.render(CssHeaderItem.forReference(new CssResourceReference(HelloWorld.class, "/css/helloworld-page-style.css")));
-	    response.render(JavaScriptReferenceHeaderItem.forReference( new JavaScriptResourceReference(HelloWorld.class, "/js/search-logic.js")));
+	    response.render(CssReferenceHeaderItem.forReference(new PackageResourceReference(HelloWorld.class, "../resources/css/helloworld-style.css")));
+	    response.render(JavaScriptReferenceHeaderItem.forReference( new PackageResourceReference(HelloWorld.class, "../resources/js/search-logic.js")));
 	}
 	
 
